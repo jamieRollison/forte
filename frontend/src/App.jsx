e9e7e1f1833728db5acc1f53b499431dabd5befc
estrella <epopo2@illinois.edu>
2023-02-25 15:04:55 -0800

    Add Music Button and setup Page component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import NavBar from './components/NavBar.jsx'
+import Post from './components/Post.jsx'
 import './App.css'
 import './assets/ringift.ttf'
 // import reactLogo from './assets/react.svg'
@@ -8,6 +9,12 @@ function App() {
   return (
     <>
    <NavBar />
+   <div className='flex justify-center'>
+    <button className='mx-10 bg-blue-200 p-2 flex-1 md:w-10 rounded-md'>
+      Add Today's Song
+    </button>
+  </div>
+  <Post />
    </>
   )
 }
