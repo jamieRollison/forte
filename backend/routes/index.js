93edbf031368d1d543e296f13ba76a61ff1bb174
Jamie Rollison <90640394+jamieRollison@users.noreply.github.com>
2023-02-25 18:56:55 -0800

    Add prettier and ESLint

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,10 +1,10 @@
-const express =  require('express');
+const express = require("express")
 
-const router = express.Router();
+const router = express.Router()
 
 // routes go here
-router.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+router.get("/", (req, res) => {
+  res.send("Hello World!")
+})
 
-module.exports = router;
+module.exports = router
