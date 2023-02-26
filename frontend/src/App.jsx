520ebad3803d99bea7deba35dd7df1082e57122b
jamieRollison <rollisonjamie15@gmail.com>
2023-02-25 18:27:51 -0800

    change header to p so it doesn't have font

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,7 +14,7 @@ function App() {
     return (<ReactModal isOpen={modalVisible} onRequestClose={() => setModalVisible(false)}
     className='w-80 h-40 mt-40 bg-black mx-auto rounded-xl'> 
     <div className='flex flex-col justify-between items-center'>
-    <h1 className='text-white text-xl text-center pt-3'>What are you listening to today?</h1>
+    <p className='text-white text-xl text-center pt-3'>What are you listening to today?</p>
     <input placeholder='search spotify' className='px-2 w-60 h-8 rounded my-4 mx-10'/>
     <button className='bg-blue-200 p-2 rounded-md w-1/2' onClick={() => setModalVisible(false)}>Add Song</button>
     </div>
