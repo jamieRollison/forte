123b1aa32ce90637614778c45d3a893fc9daca11
vasuchal <MadhaviChalasani@srikanths-MacBook-Pro-2.local>
2023-02-26 02:41:29 -0800

    Add song object

---
+++ backend/models/song.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
+
+const songSchema =  new Schema({
+    songName: {
+        type: String,
+        required: true
+    },
+    artist: {
+        type: String,
+        required: true
+    },
+    url: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+});
+
+const Song = mongoose.model("Song", songSchema, "Song")
+
+module.exports = {
+  Song,
+}
