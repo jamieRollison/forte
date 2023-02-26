facc117389ad32866cd27b577c44b605278c8a06
vasuchal <MadhaviChalasani@srikanths-MacBook-Pro-2.local>
2023-02-26 02:30:59 -0800

    Add profile settings

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -2,14 +2,19 @@ import React from "react"
 import { AiOutlineUsergroupAdd } from "react-icons/ai"
 import { AiOutlineUser } from "react-icons/ai"
 import ForteLogo from "../assets/forte-logo.png"
+import { Link } from "react-router-dom"
 
-function NavBar() {
+const NavBar = ({ userId, showIcons }) => {
   return (
     <nav className="bg-black text-white flex justify-center py-5">
       <div className="flex items-center justify-end gap-10">
-        <AiOutlineUsergroupAdd size={30} />
-        <img src={ForteLogo}></img>
-        <AiOutlineUser size={30} />
+        {showIcons && <AiOutlineUsergroupAdd size={30} />}
+        <Link to="/feed">
+          <img src={ForteLogo}></img>
+        </Link>
+        {showIcons && <Link to="/profile" state={{ userId: userId }}>
+          <AiOutlineUser size={30} />
+        </Link>}
       </div>
     </nav>
   )
