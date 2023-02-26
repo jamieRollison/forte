7be73ad2a1779ee764e5b1de281d94f72870c462
Ellie Popoca <estrellapopoca2002@gmail.com>
2023-02-25 16:28:42 -0800

    Exrlla/nav (#4)
    * Add Navigaton
    
    * Add Music Button and setup Page component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import NavBar from './components/NavBar.jsx'
+import Post from './components/Post.jsx'
 import './App.css'
+import './assets/ringift.ttf'
+// import reactLogo from './assets/react.svg'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <>
+   <NavBar />
+   <div className='flex justify-center'>
+    <button className='mx-10 bg-blue-200 p-2 flex-1 md:w-10 rounded-md'>
+      Add Today's Song
+    </button>
+  </div>
+  <Post />
+   </>
   )
 }
 
